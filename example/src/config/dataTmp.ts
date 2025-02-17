8d8b9672802aa6ef30551f7c0b1d37fdbe12ba8c
shuraman <a.fateev@stayfitt.ru>
2025-02-17 08:51:22 +0300

    feat: add dataTmp.ts for example app

---
+++ example/src/config/dataTmp.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker';
+
+export const generateStoryData = (count = 20) => {
+  return Array.from({ length: count }, (_, index) => ({
+    id: faker.string.uuid(),
+    image: faker.image.url(),
+    hello: '123',
+    title: index % 2 === 0 ? faker.lorem.words(3) : '',
+    isViewed: faker.datatype.boolean(),
+    steps: Array.from({ length: faker.number.int({ min: 2, max: 5 }) }, () => ({
+      id: faker.string.uuid(),
+      image: faker.image.url(),
+      title: faker.lorem.words(5),
+      duration: faker.number.int({ min: 3000, max: 10000 }),
+    })),
+  }));
+};
